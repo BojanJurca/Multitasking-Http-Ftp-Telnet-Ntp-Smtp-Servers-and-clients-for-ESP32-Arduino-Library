065f19c4936038e7924079c1eb6a0311bdbd33c1
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2026-01-25 09:56:31 +0100

    Add user-defined Telnet commands for LED management
    Implement user-defined Telnet commands for LED control.

---
+++ examples/Telnet_server/User_defined_Telnet_commands/User_defined_Telnet_commands.ino
@@ -0,0 +1,123 @@
+#include <WiFi.h>
+#include <ntpClient.h>            // NTP client is neede only for time commands
+#define HOSTNAME "Esp32Server"    // Choose your server's name - this is how Telnet server would introduce itself to the clients
+
+// Choose which built-in Telnet commands will be included
+#define TELNET_CLEAR_COMMAND 0      // 0=exclude, 1=include, clear included by default
+#define TELNET_UNAME_COMMAND 1      // 0=exclude, 1=include, uname included by default
+#define TELNET_FREE_COMMAND 0       // 0=exclude, 1=include, free included by default
+#define TELNET_NOHUP_COMMAND 0      // 0=exclude, 1=include, nohup included by default
+#define TELNET_REBOOT_COMMAND 1     // 0=exclude, 1=include, reboot included by default
+#define TELNET_DMESG_COMMAND 0      // 0=exclude, 1=include, dmesg included by default
+#define TELNET_QUIT_COMMAND 0       // 0=exclude, 1=include, quit included by default
+#define TELNET_UPTIME_COMMAND 0     // 0=exclude, 1=include, date included by default
+#define TELNET_DATE_COMMAND 0       // 0=exclude, 1=include, date included by default
+#define TELNET_NTPDATE_COMMAND 0    // 0=exclude, 1=include, ntpdate included by default
+#define TELNET_PING_COMMAND 0       // 0=exclude, 1=include, ping included by default
+#define TELNET_IFCONFIG_COMMAND 0   // 0=exclude, 1=include, ifconfig included by default
+#define TELNET_IW_COMMAND 0         // 0=exclude, 1=include, iw included by default
+#define TELNET_NETSTAT_COMMAND 0    // 0=exclude, 1=include, netstat included by default
+#define TELNET_KILL_COMMAND 0       // 0=exclude, 1=include, kill included by default
+#define TELNET_CURL_COMMAND 0       // 0=exclude, 1=include, curl included by default
+#define TELNET_SENDMAIL_COMMAND 0   // 0=exclude, 1=include, sendmail included by default
+#define TELNET_LS_COMMAND 0         // 0=exclude, 1=include, ls included by default
+#define TELNET_TREE_COMMAND 0       // 0=exclude, 1=include, tree included by default
+#define TELNET_MKDIR_COMMAND 0      // 0=exclude, 1=include, mkdir included by default
+#define TELNET_RMDIR_COMMAND 0      // 0=exclude, 1=include, rmdir included by default
+#define TELNET_CD_COMMAND 0         // 0=exclude, 1=include, cd included by default
+#define TELNET_PWD_COMMAND 0        // 0=exclude, 1=include, pwd included by default
+#define TELNET_CAT_COMMAND 0        // 0=exclude, 1=include, cat included by default
+#define TELNET_VI_COMMAND 0         // 0=exclude, 1=include, vi included by default
+#define TELNET_CP_COMMAND 0         // 0=exclude, 1=include, cp included by default
+#define TELNET_RM_COMMAND 0         // 0=exclude, 1=include, rm included by default
+
+
+// 1️⃣ Provide help text for user-defined commands
+#define __USER_DEFINED_TELNET_HELP_TEXT__   "\r\n  LED commands:" \
+                                            "\r\n      turn led on" \
+                                            "\r\n      turn led off" \
+                                            "\r\n      led state"
+
+#include <telnetServer.h>
+
+telnetServer_t *telnetServer = NULL;
+
+#ifndef LED_BUILTIN
+    #define LED_BUILTIN 2
+#endif
+
+
+// 2️⃣ Provide callback function that would handle user-defined commands
+String telnetCommandHandlerCallback (int argc, char *argv [], telnetServer_t::telnetConnection_t *tcn) {
+
+    #define argv0is(X) (argc > 0 && !strcmp (argv[0], X))  
+    #define argv1is(X) (argc > 1 && !strcmp (argv[1], X))
+    #define argv2is(X) (argc > 2 && !strcmp (argv[2], X))   
+
+    
+    // 3️⃣ short-running functions should return the text the Telnet server will send to the client as a response to the command
+    if (argv0is ("turn") && argv1is ("led") && argv2is ("on")) {
+            digitalWrite (LED_BUILTIN, HIGH);
+            return "Led is on";
+    } else if (argv0is ("turn") && argv1is ("led") && argv2is ("off")) {
+            digitalWrite (LED_BUILTIN, LOW);
+            return "Led is off";
+    }
+
+    // 4️⃣ long-running functions should provide a mechanism to break the loop 
+    else if (argv0is ("led") && argv1is ("state")) {
+            for (int i = 0; i < 1000; i++) {
+                char buf [6];
+                sprintf (buf, "%s", digitalRead (LED_BUILTIN) ? "on\r\n" : "off\r\n");
+                if (tcn->sendString (buf) <= 0)
+                  return "\r";
+                delay (250); 
+
+                if (tcn->peekChar ()) {
+                    tcn->recvChar ();
+                    return "\r"; // break the loop and return something different than "" to let the telnet server function know that the command has been processed
+                }
+            }
+            return "\r"; // return something different than "" to let the telnet server function know that the command has been processed
+    }
+
+
+    // 5️⃣ Unhandeled - let the Telnet server try to handle the command itself
+    return "";
+}
+
+
+void setup () {
+  Serial.begin (115200);
+  WiFi.begin ("YOUR_SSID", "YOUR_PASSWORD");
+
+
+  // 6️⃣ Create Telnet server instance with user-defined callback function
+  telnetServer = new (std::nothrow) telnetServer_t (NULL, telnetCommandHandlerCallback);// optional arguments:
+                                                                                        // const Cstring<64>& userName, const Cstring<64>& password) = NULL
+                                                                                        // String (*telnetCommandHandlerCallback) (int argc, char *argv [], telnetConnection_t *tcn) = NULL
+                                                                                        // int serverPort = 23
+                                                                                        // bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
+                                                                                        // bool runListenerInItsOwnTask = true
+                                                        
+  // Check if Telnet server instance is created && Telnet server is running
+  if (telnetServer && *telnetServer)
+    Serial.println ("Telnet server started");
+  else
+    Serial.println ("Telnet server did not start");
+
+  // Use Telent client to connect to ESP32's IP address
+  while (WiFi.localIP () == IPAddress (0, 0, 0, 0)) { // wait until we get IP from router's DHCP
+      delay (1000); 
+      Serial.println ("   ."); 
+  } 
+  Serial.print ("Got IP addess: "); Serial.println (WiFi.localIP ());
+
+
+  pinMode (LED_BUILTIN, OUTPUT);
+  // ...
+}
+
+void loop () {
+
+}
