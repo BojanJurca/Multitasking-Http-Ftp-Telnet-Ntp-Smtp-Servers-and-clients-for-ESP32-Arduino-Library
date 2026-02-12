c7a851a656c6505e93ac8f261ed5a7318a40e417
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2026-02-12 20:16:26 +0100

    Update Telnet command definitions to clarify inclusion

--- examples/Telnet_server/Telnet_server_with_restricted_access/Telnet_server_with_restricted_access.ino
+++ examples/Telnet_server/Telnet_server_with_restricted_access/Telnet_server_with_restricted_access.ino
@@ -7,33 +7,34 @@ using File = threadSafeFS::File;  // Use thread-safe wrapper for all file operat
 
 
 // Choose which built-in Telnet commands will be included
-#define TELNET_CLEAR_COMMAND 0      // 0=exclude, 1=include, clear included by default
-#define TELNET_UNAME_COMMAND 0      // 0=exclude, 1=include, uname included by default
-#define TELNET_FREE_COMMAND 0       // 0=exclude, 1=include, free included by default
-#define TELNET_NOHUP_COMMAND 0      // 0=exclude, 1=include, nohup included by default
-#define TELNET_REBOOT_COMMAND 1     // 0=exclude, 1=include, reboot included by default
-#define TELNET_DMESG_COMMAND 1      // 0=exclude, 1=include, dmesg included by default
-#define TELNET_QUIT_COMMAND 1       // 0=exclude, 1=include, quit included by default
-#define TELNET_UPTIME_COMMAND 1     // 0=exclude, 1=include, date included by default
-#define TELNET_DATE_COMMAND 1       // 0=exclude, 1=include, date included by default
-#define TELNET_NTPDATE_COMMAND 1    // 0=exclude, 1=include, ntpdate included by default
-#define TELNET_PING_COMMAND 1       // 0=exclude, 1=include, ping included by default
-#define TELNET_IFCONFIG_COMMAND 1   // 0=exclude, 1=include, ifconfig included by default
-#define TELNET_IW_COMMAND 0         // 0=exclude, 1=include, iw included by default
-#define TELNET_NETSTAT_COMMAND 0    // 0=exclude, 1=include, netstat included by default
-#define TELNET_KILL_COMMAND 0       // 0=exclude, 1=include, kill included by default
-#define TELNET_CURL_COMMAND 0       // 0=exclude, 1=include, curl included by default
-#define TELNET_SENDMAIL_COMMAND 0   // 0=exclude, 1=include, sendmail included by default
-#define TELNET_LS_COMMAND 1         // 0=exclude, 1=include, ls included by default
-#define TELNET_TREE_COMMAND 0       // 0=exclude, 1=include, tree included by default
-#define TELNET_MKDIR_COMMAND 1      // 0=exclude, 1=include, mkdir included by default
-#define TELNET_RMDIR_COMMAND 1      // 0=exclude, 1=include, rmdir included by default
-#define TELNET_CD_COMMAND 1         // 0=exclude, 1=include, cd included by default
-#define TELNET_PWD_COMMAND 1        // 0=exclude, 1=include, pwd included by default
-#define TELNET_CAT_COMMAND 0        // 0=exclude, 1=include, cat included by default
-#define TELNET_VI_COMMAND 0         // 0=exclude, 1=include, vi included by default
-#define TELNET_CP_COMMAND 0         // 0=exclude, 1=include, cp included by default
-#define TELNET_RM_COMMAND 0         // 0=exclude, 1=include, rm included by default
+#define TELNET_CLEAR_COMMAND 0      // 0=exclude, 1=include, clear is included by default
+#define TELNET_UNAME_COMMAND 0      // 0=exclude, 1=include, uname is included by default
+#define TELNET_FREE_COMMAND 0       // 0=exclude, 1=include, free is included by default
+#define TELNET_NOHUP_COMMAND 0      // 0=exclude, 1=include, nohup is included by default
+#define TELNET_REBOOT_COMMAND 1     // 0=exclude, 1=include, reboot is included by default
+#define TELNET_DMESG_COMMAND 1      // 0=exclude, 1=include, dmesg is included by default
+#define TELNET_QUIT_COMMAND 1       // 0=exclude, 1=include, quit is included by default
+#define TELNET_UPTIME_COMMAND 1     // 0=exclude, 1=include, date is included by default
+#define TELNET_DATE_COMMAND 1       // 0=exclude, 1=include, date is included by default
+#define TELNET_NTPDATE_COMMAND 1    // 0=exclude, 1=include, ntpdate is included by default
+#define TELNET_PING_COMMAND 1       // 0=exclude, 1=include, ping is included by default
+#define TELNET_IFCONFIG_COMMAND 1   // 0=exclude, 1=include, ifconfig is included by default
+#define TELNET_IW_COMMAND 0         // 0=exclude, 1=include, iw is included by default
+#define TELNET_NETSTAT_COMMAND 0    // 0=exclude, 1=include, netstat is included by default
+#define TELNET_KILL_COMMAND 0       // 0=exclude, 1=include, kill is included by default
+#define TELNET_CURL_COMMAND 0       // 0=exclude, 1=include, curl is included by default
+#define TELNET_SENDMAIL_COMMAND 0   // 0=exclude, 1=include, sendmail is included by default
+#define TELNET_LS_COMMAND 1         // 0=exclude, 1=include, ls is included by default
+#define TELNET_TREE_COMMAND 0       // 0=exclude, 1=include, tree is included by default
+#define TELNET_MKDIR_COMMAND 1      // 0=exclude, 1=include, mkdir is included by default
+#define TELNET_RMDIR_COMMAND 1      // 0=exclude, 1=include, rmdir is included by default
+#define TELNET_CD_COMMAND 1         // 0=exclude, 1=include, cd is included by default
+#define TELNET_PWD_COMMAND 1        // 0=exclude, 1=include, pwd is included by default
+#define TELNET_CAT_COMMAND 0        // 0=exclude, 1=include, cat is included by default
+#define TELNET_VI_COMMAND 0         // 0=exclude, 1=include, vi is included by default
+#define TELNET_CP_COMMAND 0         // 0=exclude, 1=include, cp is included by default
+#define TELNET_RM_COMMAND 0         // 0=exclude, 1=include, rm is included by default
+#define TELNET_LSOF_COMMAND 0       // 0=exclude, 1=include, lsof is included by default
 
 #define SWAP_DEL_AND_BACKSPACE 0    // seto to 1 to swap the meaning of these keys - this would be suitable for Putty and Linux Telnet clients
 
@@ -72,6 +73,7 @@ void setup () {
                                                                                           // int serverPort = 23
                                                                                           // bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
                                                                                           // bool runListenerInItsOwnTask = true
+                                                        
                                                         
   // Check if Telnet server instance is created && Telnet server is running
   if (telnetServer && *telnetServer)
