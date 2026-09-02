75a56aeebec74ef4f7d64516d5b286fb697f02e4
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2026-09-02 15:07:34 +0200

    Add files via upload

--- examples/FTP_server/FTP_server_with_firewall/FTP_server_with_firewall.ino
+++ examples/FTP_server/FTP_server_with_firewall/FTP_server_with_firewall.ino
@@ -1,46 +1,50 @@
 /*
   **Note:** Each example demonstrates only a specific feature or use case.  
   The complete, fully integrated server solution is available here:  
-  https://github.com/BojanJurca/Multitasking-Esp32-HTTP-FTP-Telnet-servers-for-Arduino/blob/master/PROJECT_STATE.md
+  https://github.com/BojanJurca/Multitasking-Esp32-HTTP-FTP-Telnet-servers-for-Arduino
 */
 
 
 #include <WiFi.h>
+
 #include <LittleFS.h>             // Or SPIFFS.h or FFat.h or SD.h ...
 #include <threadSafeFS.h>         // Include thread-safe wrapper since SPIFFS, LittleFS, FFat and SD file systems are not thread safe
-threadSafeFS::FS TSFS (LittleFS); // Crete thread-safe wrapper arround LittleFS (or FFat or SD)
-using File = threadSafeFS::File;  // Use thread-safe wrapper for all file operations from now on in your code
+
 #define HOSTNAME "Esp32Server"    // Choose your server's name - this is how FTP server would introduce itself to the clients
 #include <ftpServer.h>
 
-
 ftpServer_t *ftpServer = NULL;
+
 
 // 1️⃣ provide a firewall callback function to FTP server that would tell which connctions to accept and which to refuse
 bool firewallCallback (char *clientIP, char *serverIP) { 
 
   // Must be reentrant !!
 
-  // accept only connections from local network, for example 10.18.1.*
-  if (strstr (clientIP, "10.18.1.") == clientIP)
+
+  // accept only connections from local network, for example:
+  if (strstr (clientIP, "192.168.1.") == clientIP)
     return true;
   else 
     return false; 
 }
 
-
 void setup () {
   Serial.begin (115200);
-  LittleFS.begin (true);
+
+  // Start file system under (thread-safe wrapper) tsfs (LittleFS or SPIFFS or FFat or SD)
+  tsfs.begin (true);
+  
   WiFi.begin ("YOUR_SSID", "YOUR_PASSWORD");
 
 
   // 2️⃣ create FTP server instance that would use LittleFS with firewall callback function
-  ftpServer = new (std::nothrow) ftpServer_t (TSFS, NULL, 21, firewallCallback);// optional arguments:
-                                                                                //    Cstring<255> (*getUserHomeDirectory) (const Cstring<64>& userName, const Cstring<64>& password) = NULL
-                                                                                //    int serverPort = 21
-                                                                                //    bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
-                                                                                //    bool runListenerInItsOwnTask = true
+                                                                                        // Optional arguments:
+  ftpServer = new (std::nothrow) ftpServer_t (/* tsfs, */ NULL, 21, firewallCallback);  // threadSafeFS::FS& fileSystem 
+                                                                                        // Cstring<255> (*getUserHomeDirectory) (const Cstring<64>& userName, const Cstring<64>& password) = NULL
+                                                                                        // int serverPort = 21
+                                                                                        // SET THIS ARGUMENT: bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
+                                                                                        // bool runListenerInItsOwnTask = true
 
   // check if FTP server instance is created && FTP server is running
   if (ftpServer && *ftpServer)
@@ -49,7 +53,7 @@ void setup () {
     Serial.println ("FTP server did not start");
 
 
-  // ...
+  // ... your code here
 }
 
 void loop () {
